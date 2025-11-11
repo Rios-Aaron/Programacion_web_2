53bf9e93240fc76cd4cbe332c50494d0e1cd35ab
Aaron Rios <aaronrios85@aragon.umam.mx>
2025-11-10 23:57:52 -0600

    Tarea.- Aplicacion de clase con ejercicios de clase

--- Operaciones.js
+++
@@ -1,8 +0,0 @@
-var valorA = parseFloat(prompt("Ingresa el primer valor"));
-var valorB = parseFloat(prompt("Ingresa el segundo valor"));
-
-console.log("Suma : " + (valorA + valorB));
-console.log("Resta : " + (valorA - valorB));
-console.log("Multiplicación : " + (valorA * valorB));
-console.log("División : " + (valorA / valorB));
-console.log("Módulo : " + (valorA % valorB));
